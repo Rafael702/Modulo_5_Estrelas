7f641dd510de96a6eb7cc3181a2b39ef9a2cef06
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-10-27 11:51:25 -0300

    Criar Controller.

---
+++ src/main/java/br/com/zup/ZupCar/ZupCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupCarApplication.class, args);
+	}
+
+}
